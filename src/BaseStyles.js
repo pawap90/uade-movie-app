3992d1791a27a7fb07cb57e139b07888e8a63918
paulasantamaria <m_paula_santamaria@live.com>
2020-06-14 23:19:20 -0300

    Add padding to base container

--- src/BaseStyles.js
+++ src/BaseStyles.js
@@ -2,7 +2,8 @@ import { StyleSheet } from 'react-native';
 
 const BaseStyles = StyleSheet.create({
 	container: {
-		backgroundColor: '#1F2D3D'
+		backgroundColor: '#1F2D3D',
+		padding: 24
 	},
 	header: {
 		backgroundColor: '#60C7AC'
