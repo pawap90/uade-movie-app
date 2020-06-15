7a9aba5bf120506b3cf742dc5835cb7c6bf6f7df
paulasantamaria <m_paula_santamaria@live.com>
2020-06-14 23:19:02 -0300

    MediaTypeSwitch component

--- src/components/MediaTypeSwitch.js
+++ src/components/MediaTypeSwitch.js
@@ -1,36 +1,7 @@
-import React from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableWithoutFeedback, View, StyleSheet, Text } from 'react-native';
 import Constants from 'expo-constants';
-import MediaCarouselItem from './MediaCarouselItem';
 import PropTypes from 'prop-types';
-
-const DATA = [
-	{
-		id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-		title: 'Jurassic Park',
-		imageUrl: 'https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_.jpg'
-	},
-	{
-		id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-		title: 'Avengers: Endgame',
-		imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg'
-	},
-	{
-		id: '58694a0f-3da1-471f-bd96-145571e29d72',
-		title: 'Game of Thrones',
-		imageUrl: 'https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_SY1000_CR0,0,734,1000_AL_.jpg'
-	},
-	{
-		id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f64',
-		title: 'Avengers: Endgame',
-		imageUrl: 'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg'
-	},
-	{
-		id: '58694a0f-3da1-471f-bd96-145571e29d76',
-		title: 'Game of Thrones',
-		imageUrl: 'https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_SY1000_CR0,0,734,1000_AL_.jpg'
-	}
-];
 
 MediaTypeSwitch.propTypes = {
 	defaultType: PropTypes.string,
@@ -39,14 +10,13 @@ MediaTypeSwitch.propTypes = {
 };
 
 export default function MediaTypeSwitch(props) {
-	const { defaultType = 'movie', onClickMovie = () => { }, onClickSeries = () => { } } = props;
+	const { onClickMovie = () => { }, onClickSeries = () => { } } = props;
+	const [selectedType, setSelectedType] = useState('movie');
 
-	let selectedType = defaultType;
+	const switchType = (mediaType) => {
+		setSelectedType(mediaType);
 
-	const onSwitch = (mediaType) => {
-		selectedType = mediaType;
-
-		if (selectedType === 'movie') {
+		if (mediaType === 'movie') {
 			onClickMovie();
 		}
 		else {
@@ -56,35 +26,50 @@ export default function MediaTypeSwitch(props) {
 
 	return (
 		<View style={styles.container}>
-			<Button
-				onPress={onSwitch('movie')}
-				title="Películas"
-				style={selectedType === 'movie' ? styles.buttonActive : styles.buttonIdle}
-			/>
-			<Button
-				onPress={onSwitch('series')}
-				title="Series"
-				style={selectedType === 'series' ? styles.buttonActive : styles.buttonIdle}
-			/>
-		</View>
+			<TouchableWithoutFeedback
+				onPress={() => { switchType('movie') }}>
+				<View style={[selectedType === 'movie' ? styles.buttonActive : styles.buttonIdle, styles.buttonLeft]}>
+					<Text style={styles.buttonLabel}>Películas</Text>
+				</View>
+			</TouchableWithoutFeedback >
+			<TouchableWithoutFeedback
+				onPress={() => { switchType('series') }}>
+				<View style={[selectedType === 'series' ? styles.buttonActive : styles.buttonIdle, styles.buttonRight]}>
+					<Text style={styles.buttonLabel} >Series</Text>
+				</View>
+			</TouchableWithoutFeedback>
+		</View >
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flexDirection: 'row',
-		justifyContent: 'space-between',
+		justifyContent: 'flex-start',
 		alignItems: 'flex-end',
-		marginBottom: 10
+	},
+	buttonLeft: {
+		borderTopLeftRadius: 10,
+		borderBottomLeftRadius: 10
+	},
+	buttonRight: {
+		borderTopRightRadius: 10,
+		borderBottomRightRadius: 10
 	},
 	buttonIdle: {
-		color: '#FFFFFF',
 		backgroundColor: '#344250',
-		fontSize: 18,
+		paddingHorizontal: 12,
+		paddingVertical: 6,
+		flex: 1,
 	},
 	buttonActive: {
-		color: '#60C7AC',
-		backgroundColor: '#344250',
-		fontSize: 18,
+		backgroundColor: '#60C7AC',
+		paddingHorizontal: 12,
+		paddingVertical: 6,
+		flex: 1,
+	},
+	buttonLabel: {
+		color: '#FFFFFF',
+		fontSize: 16
 	}
 });
