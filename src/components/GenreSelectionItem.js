3f1577c8a17db99094ec6f20f48baa496a9f7dfb
jaime-human <jaime99_og@hotmail.com>
2020-06-20 05:39:09 -0300

    code is now repeated, maybe after i make it work i could improve it. or not..

--- src/components/GenreSelectionItem.js
+++ src/components/GenreSelectionItem.js
@@ -11,14 +11,14 @@ export default function GenreSelectionItem(props) {
 	const [selectedOrNot, setselectedOrNot] = useState(false);
 
 	const genreBoxPressed = () => {
-		if (setselectedOrNot)
+		if (selectedOrNot)
 			setselectedOrNot(false);
 		else
 			setselectedOrNot(true);
 	};
 
 	return (
-		<View style={styles.genreSection} onPress={genreBoxPressed}>
+		<View style={selectedOrNot?styles.genreSectionSelected:styles.genreSectionDefault} onPress={genreBoxPressed}>
 			<Text style={styles.genreName}>{genreName}</Text>
 		</View>
 	);
@@ -33,9 +33,14 @@ const styles = StyleSheet.create({
 		fontWeight: 'bold',
 		textTransform: 'uppercase'
 	},
-	genreSection: {
+	genreSectionDefault: {
 		backgroundColor: '#1F2D3D',
 		borderWidth: 1.5,
 		borderColor: '#60C7AC',
-	}
+	},
+	genreSectionSelected: {
+		backgroundColor: '#60C7AC',
+		borderWidth: 1.5,
+		borderColor: '#60C7AC',
+	},
 });
