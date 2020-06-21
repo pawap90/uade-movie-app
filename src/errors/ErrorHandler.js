f68252dc15193dbf619c808c38011b47983abfda
paulasantamaria <m_paula_santamaria@live.com>
2020-06-20 22:30:01 -0300

    Basic error handling

---
+++ src/errors/ErrorHandler.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import UserError from "./UserError";
+
+/**
+ * Class to turn errors into UserErrors
+ */
+export default class ErrorHandler {
+
+    /**
+     * Returns a UserError that can be exposed to the user
+     * @param {String} message UserError message
+     * @param {Error} innerError Original error (optional)
+     * @param {Number} statusCode Status code for HTTP errors (optional)
+     */
+    static handle(message, innerError, statusCode) {
+        if (innerError instanceof UserError)
+            return innerError;
+
+        return new UserError(message, innerError, statusCode);
+    }
+}
---
+++ src/errors/UserError.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * Class to handle errors and turn them into something the user is allowed to see.
+ */
+export default class UserError extends Error {
+
+    /**
+     * 
+     * @param {String} message Error message (this will most likely be seen by the user).
+     * @param {Number} statusCode Status code for HTTP related errors
+     * @param {Error} innerError Original error (this must never be seen by the user or print in the console)
+     */
+    constructor(message, innerError, statusCode) {
+        super(message)
+
+        this.name = this.constructor.name
+        this.statusCode = statusCode || 500;
+        this.innerError = innerError;
+    }
+}
