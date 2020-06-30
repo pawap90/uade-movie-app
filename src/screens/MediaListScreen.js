d75d965ff6abffa81c29edbe4059380254e0f896
plaspa <laspapedro@gmail.com>
2020-06-30 01:15:53 -0300

    fix imports

--- src/screens/MediaListScreen.js
+++ src/screens/MediaListScreen.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { View } from 'react-native';
 import PropTypes from 'prop-types';
 import BaseStyles from '../BaseStyles';
 import { useNavigation } from '@react-navigation/native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { logout, showSpinner, hideSpinner } from '../actions/application';
 
@@ -13,16 +13,20 @@ MediaListScreen.propTypes = {
 
 export default function MediaListScreen() {
     const navigation = useNavigation();
-    const [user, setUser] = useState({});
 
     const dispatch = useDispatch();
 
     const [listMedia, setListMedia] = useState([]);
 
-    const { mediaType } = route.params;
-    const { mediaSearch } = route.params;
+    /* 
+        const { mediaType } = route.params;
+        const { mediaSearch } = route.params;
+    */
+    const mediaType = 'movie';
+    const mediaSearch = 'top';
 
     useEffect(() => {
+
         dispatch(showSpinner);
         if (mediaSearch === 'top') {
             const getTopRatedMedia = async () => {
@@ -48,8 +52,7 @@ export default function MediaListScreen() {
     return (
         <View style={BaseStyles.container}>
             <View style={styles.header}>
-                <Text style={styles.title}>{list.name}</Text>
-                <Text style={styles.itemCount}>{list.itemCount} elementos</Text>
+                <Text style={styles.title}>Title</Text>
             </View>
             <FlatList
                 data={listMedia}
@@ -59,3 +62,22 @@ export default function MediaListScreen() {
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    title: {
+        color: '#FFFFFF',
+        fontSize: 24
+    },
+    itemCount: {
+        color: '#FFFFFF',
+        opacity: 0.7,
+        fontSize: 18
+    },
+    header: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 10
+    }
+});
